549c9e10f2b1dde95b2d0eede3f37fd7423c5192
Sara Abbasi <bsara91620@gmail.com>
2016-02-21 16:15:15 -0500

    first commit

---
+++ first.rb
@@ -0,0 +1,54 @@
+require 'gosu'
+require './lib/player'
+require './lib/star'
+require './lib/zorder'
+
+class GameWindow < Gosu::Window
+  def initialize
+    super 640, 480
+    self.caption = "sara's Sensual Psychadelic Game of Your Dreams"
+
+    @background_image = Gosu::Image.new("media/swirl-bg.jpg", :tileable => true)
+
+    @player = Player.new
+    @player.warp(320, 240)
+
+    @star_anim = Gosu::Image::load_tiles("media/Hot_Lips2.png", 84, 57)
+    @stars = Array.new
+    @font = Gosu::Font.new(20)
+  end
+
+  def update
+    if Gosu::button_down? Gosu::KbLeft or Gosu::button_down? Gosu::GpLeft then
+      @player.turn_left
+    end
+    if Gosu::button_down? Gosu::KbRight or Gosu::button_down? Gosu::GpRight then
+      @player.turn_right
+    end
+    if Gosu::button_down? Gosu::KbUp or Gosu::button_down? Gosu::GpButton0 then
+      @player.accelerate
+    end
+    @player.move
+    @player.collect_stars(@stars)
+
+    if rand(100) <4 and @stars.size < 25 then
+      @stars.push(Star.new(@star_anim))
+    end
+  end
+
+  def draw
+    @background_image.draw(0,0,ZOrder::Background)
+    @player.draw
+    @stars.each { |star| star.draw }
+    @font.draw("Score: #{@player.score}", 10, 10, ZOrder::UI, 1.0, 1.0, 0xff_ffff00)
+  end
+
+  def button_down(id)
+    if id == Gosu::KbEscape
+      close
+    end
+  end
+end
+
+window = GameWindow.new
+window.show
